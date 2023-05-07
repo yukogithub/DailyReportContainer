97d08851caec2f59c8d128525e5cf81964c1394f
yukogithub <netteoisii@gmail.com>
2023-05-07 20:35:48 +0900

    再提出

--- db/mysql_setup.sql
+++ db/mysql_setup.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS daily_report_container CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'devuser'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'devuser'@'%';
+CREATE DATABASE IF NOT EXISTS daily_report_system CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
